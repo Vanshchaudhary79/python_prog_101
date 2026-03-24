a6ef7f638572189133f367dcbcb9a281ed453f0f
Vansh Chaudhary <Vansh.24430@stu.upes.ac.in>
2026-03-24 20:52:22 +0530

    Create exp1.py

---
+++ exp1-vansh-590024430/exp1.py
@@ -0,0 +1,38 @@
+# Step 4: Simple print statements
+print("Hello Everyone !!!")
+print("Hello")
+print("World")
+# Step 5: Comments in Python
+# This is a comment line explaining the code
+# Step 6: Variables with different data types
+x = "Hello" # string
+num = 25 # integer
+pi = 3.14 # float
+flag = True # boolean
+# Step 7: Display variables
+print(x)
+print(num)
+print(pi)
+print(flag)
+# String concatenation
+a = "Vansh"
+b = "Chaudhary"
+print(a + " " + b)
+# More variables
+first_name = "George"
+nickname = "woody"
+last_name = "Washington"
+print(first_name + " (" + nickname + ") " + last_name)
+# Personal details
+name = "NIKUNJ"
+sap_id = "500069844"
+dob = "13 Oct 1999"
+address = "UPES\n Bidholi Campus\n Pincode: 248007"
+programme = "AI & ML"
+semester = 2
+print("NAME:", name)
+print("SAP ID:", sap_id)
+print("DATE OF BIRTH:", dob)
+print("ADDRESS:", address)
+print("Programme:", programme)
+print("Semester:", semester)
